05d230ed237afc08f72ce8ff8742c9d4b116d448
Varsha Teratipally <t.varsha016@gmail.com>
2017-05-02 04:43:26 -0600

    adding data rows

--- databasegenerationscript.sql
+++ databasegenerationscript.sql
@@ -155,7 +155,7 @@ values('FMSM8903', 3, 'FactoryManager', 'MANUFACTURING', '2015-09-09 00:00:00.00
 
 
 insert into Employee_tbl(employeeId,userId, designation,department,dateOfJoining, workContact )
-values('FESC3403', 3, 'FactoryEmployee', 'MANUFACTURING', '2015-09-09 00:00:00.00', 1234);
+values('FESC3403', 4, 'FactoryEmployee', 'MANUFACTURING', '2015-09-09 00:00:00.00', 1234);
 
 
 insert into MachineLineStatus_tbl(machinelineStatus)
@@ -194,6 +194,15 @@ insert into WorkingMachineLineStatus_tbl(lineId, employeeId, startPosition,endPo
 values(2, 'FESC3403', 20, 40, 92, 'Stock Low', 'Screen');
 insert into WorkingMachineLineStatus_tbl(lineId, employeeId, startPosition,endPosition, stock,  status, machineType)
 values(2, 'FESC3403', 100, 231, 23, 'Working', 'Battery');
+
+insert into WorkingMachineLineStatus_tbl(lineId, employeeId, startPosition,endPosition, stock,  status, machineType)
+values(2, 'FMSM8903', 100, 122, 220, 'Working', 'Electronics');
+insert into WorkingMachineLineStatus_tbl(lineId, employeeId, startPosition,endPosition, stock,  status, machineType)
+values(2, 'FMSM8903', 122, 139, 90, 'Malfunction', 'Casing');
+insert into WorkingMachineLineStatus_tbl(lineId, employeeId, startPosition,endPosition, stock,  status, machineType)
+values(2, 'FMSM8903', 20, 40, 92, 'Stock Low', 'Screen');
+insert into WorkingMachineLineStatus_tbl(lineId, employeeId, startPosition,endPosition, stock,  status, machineType)
+values(2, 'FMSM8903', 100, 231, 23, 'Working', 'Battery');
 
 
 insert into ClaimStatus_tbl(claimStatus)
